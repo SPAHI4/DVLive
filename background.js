d3f98fab3e21e033353d4e5b49b521413cb9892a
Jordan Fitz <jordanfitz56@gmail.com>
2014-07-12 13:44:14 -0600

    The new DVLive\!

--- background.js
+++ background.js
@@ -1,54 +1,28 @@
-var srlAPI = "https://api.twitch.tv/kraken/streams/dvcolgansrl",
-	normalAPI = "https://api.twitch.tv/kraken/streams/dvcolgan",
-	srlNotified = false,
-	normalNotified = false;
+var notified = false;
 
-var checkAPI = function(){
-	$.ajax({
-		url: normalAPI,
-	}).done(function(data){
-		if(data.stream != null){
-			var notification = webkitNotifications.createNotification(
-				'icon.png',
-				'DVColgan is live!',
-				'DVColgan is live with "' + data.stream.channel.status + '"'
-			);
-			notification.onclick = function(){
-				chrome.tabs.create({"url": "http://twitch.tv/dvcolgan"}, function(tab){});
-			}
-			if(!normalNotified) {
-				notification.show();
-				normalNotified = true;
-			}
-		} else {
-			normalNotified = false;
-		}
-	});
-
-	$.ajax({
-		url: srlAPI,
-	}).done(function(data){
-		if(data.stream != null){
-			var notification = webkitNotifications.createNotification(
-				'icon.png',
-				'DVColganSRL is live!',
-				'DVColganSRL is live with "' + data.stream.channel.status + '"'
-			);
-			notification.onclick = function(){
-				chrome.tabs.create({"url": "http://twitch.tv/dvcolgansrl"}, function(tab){});
-			}
-			if(!srlNotified) {
-				notification.show();
-				srlNotified = true;
-			}
-		} else {
-			srlNotified = false;
-		}
+function getStream(channel) {
+	$.getJSON("https://api.twitch.tv/kraken/streams/" + channel, function(data) {
+		update(data.stream);
 	});
 }
 
-checkAPI();
+function update(stream) {
+	if (stream != null && !notified) {
+		chrome.notifications.create(
+			"", {
+				type: "basic",
+				title: "DVColgan is live!",
+				message: "DVColgan is live with \"" + stream.channel.status + "\"",
+				iconUrl: "icon.png"
+			}, function() {}
+		);
+	}
 
-setInterval(function(){
-	checkAPI();
-}, 1000*30);
+	notified = (stream != null);
+}
+
+getStream("dvcolgan");
+
+setInterval(function() {
+	getStream("dvcolgan");
+}, 5000);
--- popup.js
+++ popup.js
@@ -1,26 +1,36 @@
-(function(){
+var $status = $(".channel .status");
 
-	var srlAPI = "https://api.twitch.tv/kraken/streams/dvcolgansrl",
-		normalAPI = "https://api.twitch.tv/kraken/streams/dvcolgan";
-
-	$.ajax({
-		url: normalAPI,
-	}).done(function(data){
-		if(data.stream != null){
-			$(".channel-status.dvcolgan").html("DVColgan is live with '" + data.stream.channel.status + "'");
-		} else {
-			$(".channel-status.dvcolgan").html("DVColgan is offline.");
-		}
+$('body').on('click', 'a', function() {
+	chrome.tabs.create({
+		url: $(this).attr('href')
 	});
+	return false;
+});
 
-	$.ajax({
-		url: srlAPI,
-	}).done(function(data){
-		if(data.stream != null){
-			$(".channel-status.dvcolgansrl").html("DVColganSRL is live with '" + data.stream.channel.status + "'");
-		} else {
-			$(".channel-status.dvcolgansrl").html("DVColganSRL is offline.");
-		}
+function getStream(channel) {
+	$.getJSON("https://api.twitch.tv/kraken/streams/" + channel, function(data) {
+		updateStatus(data.stream);
 	});
+}
 
-})();
+function showNotifiction() {
+
+}
+
+function updateStatus(stream) {
+	if (stream == null) {
+		$status.html("DVColgan is currently offline.");
+		$status.removeClass("larger");
+		return;
+	} else {
+		$status.html("DVColgan is live with \"" + stream.channel.status + "\"<br>");
+		$status.append("<a href=\"http://twitch.tv/dvcolgan\"><img src=\"" + stream.preview.medium + "\"></a>");
+		$status.addClass("larger");
+	}
+}
+
+getStream("dvcolgan");
+
+setInterval(function() {
+	getStream("dvcolgan");
+}, 5000);
